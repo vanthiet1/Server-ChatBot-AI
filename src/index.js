2322711b9a8c2f85e1f95e02bdddbcd1481712c4
van Thiết <tretrauthiet@gmail.com>
2025-04-29 14:56:08 +0700

    update

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.json('Hello World!');
 })
-app.use("/api/v1",router)
+app.use("/",router)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
